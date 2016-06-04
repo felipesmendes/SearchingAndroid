b69376a6e997187955c23701e82d07b04b64a534
Guilherme6191 <gui.vasconcelos.21@gmail.com>
2016-06-04 13:36:11 -0300

    Todas as telas, e com navegação.

--- app/src/main/java/com/home/searching/CadastroItemActivity.java
+++ app/src/main/java/com/home/searching/CadastroItemActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
 
 public class CadastroItemActivity extends AppCompatActivity {
 
@@ -16,14 +17,10 @@ public class CadastroItemActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    public void SalvarItem(View view) {
+        Intent i = new Intent(this, CercaActivity.class);
+        startActivity(i);
+    }
 }
--- app/src/main/java/com/home/searching/CercaActivity.java
+++ app/src/main/java/com/home/searching/CercaActivity.java
@@ -1,13 +1,18 @@
 package com.home.searching;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class CercaActivity extends AppCompatActivity {
+
+    private WebView mWebview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +21,27 @@ public class CercaActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        mWebview = (WebView)findViewById(R.id.webViewCerca);
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        final Activity activity = this;
+
+        //em caso de erro no webview
+        mWebview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
             }
         });
+
+        mWebview.loadUrl("https://www.google.com.br/maps");
+
     }
 
+    public void SalvarCerca(View view) {
+        //logica para salvar a cerca
+
+        Intent i = new Intent(this, ListagemActivity.class);
+        startActivity(i);
+    }
 }
