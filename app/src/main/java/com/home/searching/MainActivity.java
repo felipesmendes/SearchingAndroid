0432aef0d8f762e5bcf9a070992ccc2472855f47
Guilherme6191 <gui.vasconcelos.21@gmail.com>
2016-06-13 19:19:02 -0300

    Adicionado Firebase.

--- app/src/main/java/com/home/searching/MainActivity.java
+++ app/src/main/java/com/home/searching/MainActivity.java
@@ -9,8 +9,13 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.*;
+import com.firebase.client.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Firebase myFirebaseRef;
+    EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Firebase.setAndroidContext(this);
+        myFirebaseRef = new Firebase("https://torrid-inferno-78.firebaseio.com/");
+
+        username = (EditText)findViewById(R.id.editTxtLogin);
+
+        myFirebaseRef.child("Item").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                username.setText((String) snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+            }
+            @Override public void onCancelled(FirebaseError error) { }
+        });
     }
 
     @Override
@@ -43,6 +60,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Login(View view) {
+
+        EditText username = (EditText)findViewById(R.id.editTxtLogin);
+        // Stores Password
+        EditText password = (EditText)findViewById(R.id.editTxtSenha);
+
+
+
+        // Validates the User name and Password for admin, admin
+        //if (username.getText().equals("admin") && password.getText().equals("admin")) {}
+
         Intent i = new Intent(MainActivity.this, ListagemActivity.class);
         startActivity(i);
     }
